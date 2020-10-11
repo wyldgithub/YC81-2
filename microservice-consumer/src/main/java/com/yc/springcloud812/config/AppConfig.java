2f4db56a7f093d517abe693d392242d80313f2b9
yableiwang <1772480856@qq.com>
2020-10-11 19:36:12 +0800

    provider设置权限

--- microservice-consumer/src/main/java/com/yc/springcloud812/config/AppConfig.java
+++ microservice-consumer/src/main/java/com/yc/springcloud812/config/AppConfig.java
@@ -3,7 +3,11 @@ package com.yc.springcloud812.config;
 import org.springframework.boot.SpringBootConfiguration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 //一个配置类:由spring托管
 @SpringBootConfiguration
@@ -13,4 +17,15 @@ public class AppConfig {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public HttpHeaders getHeaders(){
+        HttpHeaders headers=new HttpHeaders();
+        String auth="admin:a";   //认证的原始用户名和密码
+        byte[] encodeAuth= Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII"))); //加密处理
+        String authHeader="Basic "+new String(encodeAuth);
+        headers.set("Authorization",authHeader);    //    Http请求头         Authorization: Base xxxxxxxxx
+        return headers;
+    }
+
 }
--- microservice-consumer/src/main/java/com/yc/springcloud812/controller/BookController.java
+++ microservice-consumer/src/main/java/com/yc/springcloud812/controller/BookController.java
@@ -3,6 +3,9 @@ package com.yc.springcloud812.controller;
 
 import com.yc.springcloud812.domain.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,15 +22,21 @@ public class BookController {
     private RestTemplate restTemplate;
 
 
+    @Autowired
+    private HttpHeaders httpHeaders;
 
     @GetMapping("{id}")
-    public Book getBook(@PathVariable("id" ) Integer id ){
-
-        return restTemplate.getForObject(url+id,Book.class);
+    public Book getBook(@PathVariable("id") Integer id){
+        //return restTemplate.getForObject(URL+id,Book.class);
+        //   HttpEntity(    MultiValueMap)   ,    HttpHeaders: MultiValueMap
+        //   返回: ResponseEntity
+        return restTemplate.exchange( url+id, HttpMethod.GET, new HttpEntity<Object>(  httpHeaders   ), Book.class ).getBody();
     }
-   @GetMapping("findAll")
+
+    @GetMapping("/findAll")
     public List<Book> findAll( ){
-       return restTemplate.getForObject(url+"findAll",List.class);
-       };
+        // return restTemplate.getForObject(URL+"findAll",   List.class);
+        return restTemplate.exchange(url+"findAll",HttpMethod.GET, new HttpEntity<Object>(  httpHeaders ), List.class).getBody();
+    }
 
 }
